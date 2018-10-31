c72c2ec0921870cf34c8bb83e08b1401cc1775cb
Jason McCallister <jason.mccallister@traderinteractive.com>
2018-10-31 08:39:49 -0400

    Cleans up the server and api into packages

---
+++ api/api.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jasonmccallister/go_game/models"
+	"github.com/jinzhu/gorm"
+)
+
+type server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+// New will create a new server struct for the API configuration
+func New(conn string) (server, error) {
+	s := server{
+		Router: mux.NewRouter(),
+	}
+	db, err := gorm.Open("mysql", conn)
+
+	if err != nil {
+		return s, err
+	}
+
+	s.DB = db
+
+	return s, nil
+}
+
+func (s *server) routes() {
+	s.Router.HandleFunc("/players", s.IndexPlayers).Methods("GET")
+	s.Router.HandleFunc("/players/{id}", s.ShowPlayer).Methods("GET")
+	s.Router.HandleFunc("/players", s.CreatePlayer).Methods("POST")
+	s.Router.HandleFunc("/players/{id}", s.DeletePlayer).Methods("DELETE")
+}
+
+func (s *server) IndexPlayers(w http.ResponseWriter, r *http.Request) {
+	var player []models.Player
+	s.DB.Find(&player)
+	json.NewEncoder(w).Encode(&player)
+}
+
+func (s *server) ShowPlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var player models.Player
+	s.DB.First(&player, params["id"])
+	json.NewEncoder(w).Encode(&player)
+}
+
+func (s *server) CreatePlayer(w http.ResponseWriter, r *http.Request) {
+	var player models.Player
+	json.NewDecoder(r.Body).Decode(&player)
+	s.DB.Create(&player)
+	json.NewEncoder(w).Encode(&player)
+}
+
+func (s *server) DeletePlayer(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var player models.Player
+	s.DB.First(&player, params["id"])
+	s.DB.Delete(&player)
+
+	var players []models.Player
+	s.DB.Find(&players)
+	json.NewEncoder(w).Encode(&players)
+}
--- main.go
+++ main.go
@@ -1,98 +1,24 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	"github.com/jasonmccallister/go_game/api"
+	"github.com/jasonmccallister/go_game/models"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type Game []struct {
-	ID          int64 `jsonapi:"primary,game_id"`
-	Board       int   `jsonapi:"attr,board"`
-	WhitePlayer struct {
-		Player
-	} `jsonapi:"white_player"`
-	BlackPlayer struct {
-		Player
-	} `jsonapi:"black_player"`
-	Moves []struct {
-		Move
-	} `jsonapi:"moves"`
-	Winner string `jsonapi:"winner"`
-	Loser  string `jsonapi:"loser"`
-}
-
-type Move struct {
-	ID     int64  `jsonapi:"id"`
-	GameID int64  `jsonapi:"game_id"`
-	X      string `jsonapi:"x"`
-	Y      string `jsonapi:"y"`
-}
-
-type Player struct {
-	ID       int64  `jsonapi:"id"`
-	Name     string `jsonapi:"name"`
-	Handicap string `jsonapi:"handicap"`
-	Rating   string `jsonapi:"rating"`
-}
-
-var db *gorm.DB
-var err error
-
 func main() {
-	router := mux.NewRouter()
-
-	db, err = gorm.Open("mysql", "gotest:gotest@local_gotest?charset=utf8&parseTime=True&loc=Local")
-
+	server, err := api.New("gotest:gotest@db/local_gotest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		fmt.Printf("failed to connect database because %s", err)
+		log.Fatal(err)
 	}
 
-	defer db.Close()
+	defer server.DB.Close()
 
-	db.AutoMigrate(&Player{})
+	server.DB.AutoMigrate(&models.Player{})
 
-	router.HandleFunc("/players", IndexPlayers).Methods("GET")
-	router.HandleFunc("/players/{id}", ShowPlayer).Methods("GET")
-	router.HandleFunc("/players", CreatePlayer).Methods("POST")
-	router.HandleFunc("/players/{id}", DeletePlayer).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":80", router))
-}
-
-func IndexPlayers(w http.ResponseWriter, r *http.Request) {
-	var player []Player
-	db.Find(&player)
-	json.NewEncoder(w).Encode(&player)
-}
-
-func ShowPlayer(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var player Player
-	db.First(&player, params["id"])
-	json.NewEncoder(w).Encode(&player)
-}
-
-func CreatePlayer(w http.ResponseWriter, r *http.Request) {
-	var player Player
-	json.NewDecoder(r.Body).Decode(&player)
-	db.Create(&player)
-	json.NewEncoder(w).Encode(&player)
-}
-
-func DeletePlayer(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var player Player
-	db.First(&player, params["id"])
-	db.Delete(&player)
-
-	var players []Player
-	db.Find(&players)
-	json.NewEncoder(w).Encode(&players)
+	log.Fatal(http.ListenAndServe(":80", server.Router))
 }
