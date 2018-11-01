82ddad2b485d956bb08d5080ff3bc57944d96f96
Jason McCallister <themccallister@gmail.com>
2018-10-31 21:03:03 -0400

    Adds started test on the API package

--- api/api_test.go
+++ api/api_test.go
@@ -1,1 +1,28 @@
 package api_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/realtimdunbar/go_game/api"
+)
+
+func TestIndexPlayers(t *testing.T) {
+	// Arrange
+	s, err := api.New("sqlite", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s.Routes()
+
+	// Act
+	req, err := http.NewRequest(http.MethodGet, "players", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Assert
+	if req.Response.StatusCode != http.StatusOK {
+		t.Errorf("Expected 200, got %v instead", req.Response.StatusCode)
+	}
+}
